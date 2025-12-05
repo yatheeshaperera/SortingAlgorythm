d32e141121e9f3e542aadefe72660112075fdd52
Yatheesha Perera <85598263+yatheeshaperera@users.noreply.github.com>
2025-12-05 15:23:53 +0530

    Update bubblesorttest.py
    Bubble Sort algorithm python

--- bubblesorttest.py
+++ bubblesorttest.py
@@ -1,13 +1,13 @@
-l=[]
-while True:
+l=[] #list name
+while True: #runs until the below condition true
     num=int(input(""))
     if num>0:
         l.append(num)
     else:
         break
 loop=0
-a=0
-b=1
+a=0 #variable
+b=1 #variable
 cache=0
 while loop<len(l):
     a=0
@@ -22,6 +22,7 @@ while loop<len(l):
     loop=loop+1
     continue
 print(l)
+
 
 
             
